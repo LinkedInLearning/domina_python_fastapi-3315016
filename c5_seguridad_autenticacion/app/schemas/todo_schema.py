2fb64cf159ff473b160a95c9a5b71b65ccc91919
Ana Maria Pinto <47826699+apinto25@users.noreply.github.com>
2023-11-19 23:44:18 -0500

    Update files

--- c5_seguridad_autenticacion/app/schemas/todo_schema.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class TodoSchema(BaseModel):
-    title: str = Field(min_length=3, max_length=100)
-    description: str = Field(min_length=3, max_length=500)
-    priority: int = Field(gt=0, lte=5)
-    complete: bool
